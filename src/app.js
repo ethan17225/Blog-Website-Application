4bec3f8f18db43442728cb5bc51c7aaa1320d9a0
hai17225 <nth17225@gmail.com>
2024-09-14 20:15:45 +0000

    Add Footer and /blogs/:id route

--- src/app.js
+++ src/app.js
@@ -6,7 +6,13 @@ const mustacheExpress = require('mustache-express');
 const BLOGS = require('./blogs');
 
 const app = express();
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), {
+    setHeaders: (res, path, stat) => {
+      if (path.endsWith('.css')) {
+        res.set('Content-Type', 'text/css');
+      }
+    }
+  }));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Configure mustache
@@ -17,12 +23,13 @@ app.engine('mustache', mustacheExpress());
 // Render the template
 app.get('/', (req, res) => {
     res.render("index", { blogs: BLOGS});
+});
+
+app.get('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+    const matchedBlog = BLOGS.find(blog => blog.id.toString() === id);
+    res.render('blog', { blog: matchedBlog, content: matchedBlog.content });
 })
-
-
-
-
-
 
 
 const port = process.env.PORT || 3000;
